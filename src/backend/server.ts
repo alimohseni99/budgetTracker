b1f2a93220096714832291a5a6060d241756dd51
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-10-12 23:41:27 +0200

    front and backend separated

--- src/backend/server.ts
+++
@@ -1,9 +0,0 @@
-import http from "http";
-
-const server = http.createServer((req, res) => {
-  res.end("Hello World");
-});
-
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
