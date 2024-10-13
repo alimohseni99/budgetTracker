cd6cbc22b09adec76db5e04bb678a24c4388d2aa
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-10-13 16:13:04 +0200

    added test for calculateTransacions

--- backend/src/budget.test.ts
+++ backend/src/budget.test.ts
@@ -1,9 +1,19 @@
-import { calculateBudget } from "./budget";
+import {
+  calculateBudget,
+  calculateTransactions,
+  getTransactions,
+} from "./budget";
 
 describe("calculateBudget", () => {
   it("should calculate the remaining budget", () => {
     const budget = { budget: 100, transactions: [10, 20, 30] };
     const remaining = calculateBudget(budget);
     expect(remaining).toBe(40);
+  });
+
+  it("Should calculate the total of transactions", () => {
+    const transactions = [10, 20, 30];
+    const total = calculateTransactions(transactions);
+    expect(total).toBe(60);
   });
 });
