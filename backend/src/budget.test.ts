1ea579e2a4dbbadc9b26b04e9fa6672403208162
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-10-13 16:19:12 +0200

    added test for throw error

--- backend/src/budget.test.ts
+++ backend/src/budget.test.ts
@@ -10,6 +10,15 @@ describe("calculateBudget", () => {
     const remaining = calculateBudget(budget);
     expect(remaining).toBe(40);
   });
+  it("should throw an error if transactions is not an array", () => {
+    const budget = { budget: 100, transactions: 100 as unknown as number[] };
+
+    const result = () => calculateBudget(budget);
+
+    expect(() => calculateBudget(budget)).toThrow(
+      "transactions should be an array"
+    );
+  });
 
   it("Should calculate the total of transactions", () => {
     const transactions = [10, 20, 30];
